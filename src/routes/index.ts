4ca75fa008d503dfa4765951f659f9792e060b38
anho <anhocva214@gmail.com>
2021-09-16 01:23:57 +0700

    update apis heart rate

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import UserController from '@controllers/user.controller';
 import {adminMiddleware, userMiddleware} from '@middleware/jwt.middleware';
 import AdminController from '@controllers/admin.controller';
+import HeartRateController from '@controllers/heart-rate.controller';
 
 
 
@@ -22,13 +23,19 @@ adminRouter.post('/create', adminController.create.bind(adminController));
 adminRouter.post('/login', adminController.login.bind(adminController));
 adminRouter.get('/authenticate', adminMiddleware, adminController.authenticate.bind(adminController));
 
+// Senser-route
+const heartRouter = Router();
+const heartRateController = new HeartRateController();
+heartRouter.post('/create', heartRateController.create.bind(heartRateController));
+heartRouter.get('/list', heartRateController.list.bind(heartRateController));
+
 
 
 // Export the base-router
 const baseRouter = Router();
 baseRouter.use('/user', userRouter);
 baseRouter.use('/admin', adminRouter);
-
+baseRouter.use('/sensor/heart-rate', heartRouter);
 
 
 

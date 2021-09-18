12cfae7dc829dfe309ebb815e2b487b4d2198b64
anho <anhocva214@gmail.com>
2021-09-18 16:24:53 +0700

    update api get latest

--- push.py
+++ push.py
@@ -7,5 +7,5 @@ msg = input("Enter message: ")
 
 os.system("git add .")
 os.system("""git commit -m " """+msg+""" " """)
-os.system("git push origin master")
+os.system("git push origin sensor-apis")
 # os.system("git push heroku master")

0ade1d553b6580cf097ce2bf16bef68dafc4e3bd
anho <anhocva214@gmail.com>
2021-09-18 15:30:52 +0700

    09/18/2021, 15:30:49 update ino

--- arduino.ino
+++ arduino.ino
@@ -30,66 +30,49 @@ void loop()
 
     //  HTTPClient http;
 
-    //  http.begin("https://foresy-apis-sensor.herokuapp.com/sensor/heart-rate/create");  //Specify destination for HTTP request
-    //  http.addHeader("Content-Type", "application/json");             //Specify content-type header
+    http.begin("https://foresy-apis-sensor.herokuapp.com/sensor/heart-rate/create"); //Specify destination for HTTP request
+    http.addHeader("Content-Type", "application/json");                              //Specify content-type header
 
-    //  int httpResponseCode = http.POST("{\"device_id\":\"bb789020-1653-11ec-9c42-bb601b2bea72\",\"value\":\"70\",\"timestamp\":\"163456789\"}");   //Send the actual POST request
+    String response = http.getString(); //Get the response to the request
 
-    //  if(httpResponseCode>0){
+      Serial.println(httpResponseCode); //Print return code
+      Serial.println(response);         //Print request answer
 
-    //   String response = http.getString();                       //Get the response to the request
+      Serial.println("—————");
+      StaticJsonBuffer<300> JSONbuffer;
+      JsonObject &JsonDataSensor = JSONbuffer.createObject();
 
-    //   Serial.println(httpResponseCode);   //Print return code
-    //   Serial.println(response);           //Print request answer
+      JsonDataSensor["device_id"] = "bb789020-1653-11ec-9c42-bb601b2bea72";
+      JsonDataSensor["value"] = 99;
+      JsonDataSensor["timestamp"] = 163456789;
+     
+      // int lenghtSimple = JsonDataSensor.measureLength();
+      // Serial.print("Less overhead JSON message size: ");
+      // Serial.println(lenghtSimple);
 
-    Serial.println("—————");
-    StaticJsonBuffer<300> JSONbuffer;
-    JsonObject &JSONencoder = JSONbuffer.createObject();
+      // int lenghtPretty = JsonDataSensor.measurePrettyLength();
+      // Serial.print("Pretty JSON message size: ");
+      // Serial.println(lenghtPretty);
+      // Serial.println();
 
-    JSONencoder["sensorType"] = "Temperature";
-    JsonArray &values = JSONencoder.createNestedArray("values");
+      // Serial.println("Less overhead JSON message: ");
+      // JsonDataSensor.printTo(Serial);
+      // Serial.println();
 
-    values.add(20);
-    values.add(21);
-    values.add(23);
+      // Serial.println("\nPretty JSON message: ");
+      // JsonDataSensor.prettyPrintTo(Serial);
+      // Serial.println();
 
-    int lenghtSimple = JSONencoder.measureLength();
-    Serial.print("Less overhead JSON message size: ");
-    Serial.println(lenghtSimple);
+    int httpResponseCode = http.POST(JsonDataSensor); //Send the actual POST request
 
-    int lenghtPretty = JSONencoder.measurePrettyLength();
-    Serial.print("Pretty JSON message size: ");
-    Serial.println(lenghtPretty);
-    Serial.println();
+    Serial.println(httpResponseCode);
+    http.end(); //Free resources
 
-    Serial.println("Less overhead JSON message: ");
-    JSONencoder.printTo(Serial);
-    Serial.println();
+    else
+    {
 
-    Serial.println("\nPretty JSON message: ");
-    JSONencoder.prettyPrintTo(Serial);
-    Serial.println();
+      Serial.println("Error in WiFi connection");
+    }
 
-    Serial.println("\nPretty JSON message from buffer: ");
-    char JSONmessageBuffer[300];
-    JSONencoder.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
-    Serial.println(JSONmessageBuffer);
-    Serial.println();
+    delay(10000); //Send a request every 10 seconds
   }
-  // else
-  // {
-
-  //   Serial.print("Error on sending POST: ");
-  //   // Serial.println(httpResponseCode);
-  // }
-
-  // http.end(); //Free resources
-
-  else
-  {
-
-    Serial.println("Error in WiFi connection");
-  }
-
-  delay(10000); //Send a request every 10 seconds
-}
